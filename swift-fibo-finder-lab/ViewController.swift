90a0c2c3991006c056ec4ca2df23f24e889d9e7f
MichelleBKlynn <michellestaton@gmail.com>
2016-09-22 17:15:35 -0400

    Complete answers

--- swift-fibo-finder-lab/ViewController.swift
+++ swift-fibo-finder-lab/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func fiboFinder(index: Int) -> [Int] {
+        var fiboArray = [Int]()
+        var firstNumber = 0
+        var secondNumber = 1
+        
+        for _ in 0...index{
+            (firstNumber, secondNumber) = (secondNumber, firstNumber + secondNumber)
+            fiboArray.append(firstNumber)
+        }
+        return fiboArray
+    }
+    
 }
 
